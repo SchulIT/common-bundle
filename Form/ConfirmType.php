d9f2b1ba5c2f25edb7455eb9ad72683b56b47183
Marcel Marnitz <marcelmarnitz@googlemail.com>
2018-05-19 13:18:14 +0200

    Bugfix

--- Form/ConfirmType.php
+++ Form/ConfirmType.php
@@ -2,7 +2,6 @@
 
 namespace SchoolIT\CommonBundle\Form;
 
-use App\Form\FieldsetType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\FormBuilderInterface;
