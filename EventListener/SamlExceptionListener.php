c00c142f419c7602ba2d24b05c14861d3549f385
Marcel Marnitz <marcelmarnitz@googlemail.com>
2020-02-14 12:55:41 +0100

    Adds SamlExceptionListener and therefor moves sign in template to this bundle

---
+++ EventListener/SamlExceptionListener.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace SchoolIT\CommonBundle\EventListener;
+
+use LightSaml\Error\LightSamlBindingException;
+use LightSaml\Error\LightSamlContextException;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Twig\Environment;
+
+class ExceptionListener implements EventSubscriberInterface {
+
+    private $retryRoute;
+    private $twig;
+
+    public function __construct(string $retryRoute, Environment $twig) {
+        $this->retryRoute = $retryRoute;
+        $this->twig = $twig;
+    }
+
+    public function onKernelException(ExceptionEvent $event) {
+        $throwable = $event->getThrowable();
+
+        if($throwable instanceof LightSamlContextException || $throwable instanceof LightSamlBindingException) {
+            $response = new Response(
+                $this->twig->render('@Common/exception/saml.html.twig', [
+                    'route' => $this->retryRoute
+                ])
+            );
+
+            $event->setResponse($response);
+        }
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public static function getSubscribedEvents() {
+        return [
+            KernelEvents::EXCEPTION => [
+                ['onKernelException', 0 ]
+            ]
+        ];
+    }
+}
---
+++ Security/User/AbstractUserMapper.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace SchoolIT\CommonBundle\Security\User;
+
+use LightSaml\Model\Protocol\Response;
+
+class AbstractUserMapper {
+    protected const ROLES_ASSERTION_NAME = 'urn:roles';
+
+    /**
+     * @param Response $response
+     * @param string[] $valueAttributes
+     * @param string[] $valuesAttributes
+     * @return array
+     */
+    protected function transformResponseToArray(Response $response, array $valueAttributes, array $valuesAttributes) {
+        $result = [ ];
+
+        foreach($valueAttributes as $valueAttribute) {
+            if($this->hasAttribute($response, $valueAttribute)) {
+                $result[$valueAttribute] = $this->getValue($response, $valueAttribute);
+            }
+        }
+
+        foreach($valuesAttributes as $valuesAttribute) {
+            if($this->hasAttribute($response, $valuesAttribute)) {
+                $result[$valuesAttribute] = $this->getValues($response, $valuesAttribute);
+            }
+        }
+
+        return $result;
+    }
+
+    private function hasAttribute(Response $response, $attributeName) {
+        return $response->getFirstAssertion() !== null
+            && $response->getFirstAssertion()->getFirstAttributeStatement() !== null
+            && $response->getFirstAssertion()->getFirstAttributeStatement()->getFirstAttributeByName($attributeName) !== null;
+    }
+
+    private function getValue(Response $response, $attributeName) {
+        return $response->getFirstAssertion()->getFirstAttributeStatement()
+            ->getFirstAttributeByName($attributeName)->getFirstAttributeValue();
+    }
+
+    private function getValues(Response $response, $attributeName) {
+        return $response->getFirstAssertion()->getFirstAttributeStatement()
+            ->getFirstAttributeByName($attributeName)->getAllAttributeValues();
+    }
+}
