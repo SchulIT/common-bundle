49e54fc619f2f95e75d37a69ae47d63e9833701c
Marcel Marnitz <marcelmarnitz@googlemail.com>
2020-06-21 16:25:06 +0200

    Adds logout view

---
+++ Controller/LogoutController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace SchoolIT\CommonBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class LogoutController extends AbstractController {
+
+    private $logoutUrl;
+
+    public function __construct(string $logoutUrl) {
+        $this->logoutUrl = $logoutUrl;
+    }
+
+    public function logout() {
+        return $this->render('@Common/auth/logout.html.twig', [
+            'logout_url' => $this->logoutUrl
+        ]);
+    }
+}
