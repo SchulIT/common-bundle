03a91174a98879f89fdd700633bfe9404460a968
Marcel Marnitz <marcelmarnitz@googlemail.com>
2020-06-29 17:19:05 +0200

    Adds ability to specify multiple certificate in CreateCertificateCommand

--- Command/CreateCertificateCommand.php
+++ Command/CreateCertificateCommand.php
@@ -3,30 +3,50 @@
 namespace SchoolIT\CommonBundle\Command;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
 class CreateCertificateCommand extends Command {
 
-    private $keyFile;
-    private $certFile;
+    private $types = [ ];
 
-    public function __construct($keyFile, $certFile, ?string $name = null) {
+    public function __construct(array $types, ?string $name = null) {
         parent::__construct($name);
 
-        $this->keyFile = $keyFile;
-        $this->certFile = $certFile;
+        $this->types = $types;
     }
 
     public function configure() {
         $this
             ->setName('app:create-certificate')
-            ->setDescription('Creates a new self-signed certificate');
+            ->setDescription('Creates a new self-signed certificate')
+            ->addOption(
+                'type',
+                't',
+                InputOption::VALUE_REQUIRED,
+                'Type of certificate (e.g. saml or oauth2)'
+            );
     }
 
     public function run(InputInterface $input, OutputInterface $output) {
         $io = new SymfonyStyle($input, $output);
+
+        $type = $input->getOption('type');
+        $availableTypes = array_keys($this->types);
+
+        if(!in_array($type, $availableTypes)) {
+            $io->error('Invalid certificate type - available types: ' . implode(', ', $availableTypes));
+            return 1;
+        }
+
+        dump($type);
+        dump($this->types[$type]);
+
+        $keyFile = $this->types[$type]['keyFile'];
+        $certFile = $this->types[$type]['certFile'];
 
         $config = [
             'digest_alg' => 'sha512',
@@ -40,10 +60,10 @@ class CreateCertificateCommand extends Command {
         $data['countryName'] = $io->ask('countryName', 'DE');
         $data['stateOrProvinceName'] = $io->ask('stateOrProvinceName', 'Nordrhein-Westfalen');
         $data['localityName'] = $io->ask('localityName', 'Aachen');
-        $data['organizationName'] = $io->ask('organizationName', 'SchoolIT');
-        $data['organizationalUnitName'] = $io->ask('organizationalUnitName', 'SchoolIT Development');
-        $data['commonName'] = $io->ask('commonName', 'sso.school.it');
-        $data['emailAddress'] = $io->ask('emailAddress', 'admin@school.it');
+        $data['organizationName'] = $io->ask('organizationName', 'SchulIT');
+        $data['organizationalUnitName'] = $io->ask('organizationalUnitName', 'SchulIT IT');
+        $data['commonName'] = $io->ask('commonName', 'sso.schulit.de');
+        $data['emailAddress'] = $io->ask('emailAddress', 'admin@schoolit.de');
 
         $csr = openssl_csr_new($data, $privKey, $config);
         $cert = openssl_csr_sign($csr, null, $privKey, 10*365, $config);
@@ -51,12 +71,13 @@ class CreateCertificateCommand extends Command {
         openssl_x509_export($cert, $certout);
         openssl_pkey_export($privKey, $keyout);
 
-        file_put_contents($this->keyFile, $keyout);
-        file_put_contents($this->certFile, $certout);
+        file_put_contents($keyFile, $keyout);
+        file_put_contents($certFile, $certout);
 
         openssl_x509_free($cert);
         openssl_pkey_free($privKey);
 
-        $io->success(sprintf('Certificate generated successfully (saved as %s)', $this->certFile));
+        $io->success(sprintf('Certificate generated successfully (saved as %s)', $certFile));
+        return 0;
     }
 }
