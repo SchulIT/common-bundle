5ebd9ec1057dda36606bbf855bca83b0a80fcce5
Marcel Marnitz <marcelmarnitz@googlemail.com>
2020-07-15 13:16:33 +0200

    New command/cronjob: remove logs after seven days + clean up services.yaml

---
+++ Command/ClearLogsCommand.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace SchulIT\CommonBundle\Command;
+
+use Doctrine\ORM\EntityManagerInterface;
+use SchulIT\CommonBundle\Entity\LogEntry;
+use SchulIT\CommonBundle\Helper\DateHelper;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class ClearLogsCommand extends Command {
+
+    private const KeepLogsForDays = 7;
+
+    private $dateHelper;
+    private $em;
+
+    public function __construct(DateHelper $dateHelper, EntityManagerInterface $em, string $name = null) {
+        parent::__construct($name);
+
+        $this->dateHelper = $dateHelper;
+        $this->em = $em;
+    }
+
+    public function configure() {
+        $this
+            ->setName('app:clear-logs')
+            ->setDescription('Clears all logs older than seven days.');
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output) {
+        $threshold = $this->dateHelper->getToday()->modify(sprintf('-%d days', static::KeepLogsForDays));
+
+        $style = new SymfonyStyle($input, $output);
+        $style->section(sprintf('Delete log entries older than %s...', $threshold->format('c')));
+
+        $count = $this->em->createQueryBuilder()
+            ->delete(LogEntry::class, 'l')
+            ->where('l.time < :threshold')
+            ->setParameter('threshold', $threshold)
+            ->getQuery()
+            ->execute();
+
+        $style->success(sprintf('Deleted %d entries', $count));
+
+        return 0;
+    }
+}
