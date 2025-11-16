57e2e3c0f9f8a29c74183e1a147b2badd8d0a9d3
Marcel Marnitz <marcelmarnitz@googlemail.com>
2025-11-16 19:30:02 +0100

    Resolve deprecation by using invokable command

--- Command/ClearLogsCommand.php
+++ Command/ClearLogsCommand.php
@@ -7,23 +7,19 @@ use SchulIT\CommonBundle\Entity\LogEntry;
 use SchulIT\CommonBundle\Helper\DateHelper;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
 #[AsCommand(name: 'app:clear-logs', description: 'Clears all logs older than seven days.')]
-class ClearLogsCommand extends Command {
+class ClearLogsCommand {
 
-    private const KeepLogsForDays = 7;
+    private const int KeepLogsForDays = 7;
 
-    public function __construct(private readonly DateHelper $dateHelper, private readonly EntityManagerInterface $em, string $name = null) {
-        parent::__construct($name);
-    }
+    public function __construct(
+        private readonly DateHelper $dateHelper,
+        private readonly EntityManagerInterface $em) { }
 
-    public function execute(InputInterface $input, OutputInterface $output): int {
+    public function __invoke(SymfonyStyle $style): int {
         $threshold = $this->dateHelper->getToday()->modify(sprintf('-%d days', static::KeepLogsForDays));
-
-        $style = new SymfonyStyle($input, $output);
         $style->section(sprintf('Lösche Log-Einträge, die älter als %s sind...', $threshold->format('c')));
 
         $count = $this->em->createQueryBuilder()
