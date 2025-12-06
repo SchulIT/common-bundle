13cacd071622943a7860489b37aa091e186995ea
Marcel Marnitz <marcelmarnitz@googlemail.com>
2025-12-06 13:56:18 +0100

    Correctly use inputs as arguments instead of options

--- src/Command/CreateCertificateInput.php
+++ src/Command/CreateCertificateInput.php
@@ -2,39 +2,39 @@
 
 namespace SchulIT\CommonBundle\Command;
 
+use Symfony\Component\Console\Attribute\Argument;
 use Symfony\Component\Console\Attribute\Ask;
-use Symfony\Component\Console\Attribute\Option;
 
 class CreateCertificateInput {
-    #[Option('Art des Zertifikats', 'type', 't')]
-    #[Ask('Art des Zertifikats')]
+    #[Argument('Art des Zertifikats', 'type')]
+    #[Ask('Art des Zertifikats', default: 'saml')]
     public string $type;
 
-    #[Option('Land', 'countryName', 'c')]
+    #[Argument('Land', 'countryName')]
     #[Ask('Land', default: 'DE')]
     public string $countryName;
 
-    #[Option('Bundesland', 'stateOrProvinceName', 's')]
+    #[Argument('Bundesland', 'stateOrProvinceName')]
     #[Ask('Bundesland', default: 'Nordrhein-Westfalen')]
     public string $stateOrProvinceName;
 
-    #[Option('Stadt', 'localityName', 'l')]
+    #[Argument('Stadt', 'localityName')]
     #[Ask('Stadt', default: 'Aachen')]
     public string $localityName;
 
-    #[Option('Name der Organisation', 'organizationName', 'o')]
+    #[Argument('Name der Organisation', 'organizationName')]
     #[Ask('Name der Organisation', default: 'SchulIT')]
     public string $organizationName;
 
-    #[Option('Name der Organisationseinheit', 'organizationalUnitName', 'ou')]
+    #[Argument('Name der Organisationseinheit', 'organizationalUnitName')]
     #[Ask('Art des Zertifikats', default: 'SchulIT IT-Department')]
     public string $organizationalUnitName;
 
-    #[Option('Common Name', 'commonName', 'cn')]
+    #[Argument('Common Name', 'commonName')]
     #[Ask('Common Name', default: 'schulit.de')]
     public string $commonName;
 
-    #[Option('E-Mail-Adresse', 'emailAddress', 'e')]
+    #[Argument('E-Mail-Adresse', 'emailAddress')]
     #[Ask('E-Mail-Adresse', default: 'admin@schulit.de')]
     public string $emailAddress;
 }
